cd98dc1cfcb65dc1f0c0c66ee3feb72901a23007
Faisal Rashid <106532056+Faisal0619@users.noreply.github.com>
2022-11-28 22:13:46 +0530

    Add files via upload

---
+++ p1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float g,cf;   // g=GALLONS,cf=CUBIC FEET
+	cout<<"enter a number of gallons"<<endl;
+	cin>>g;
+	cf=g/7.481;//7.481 gallons in a cubic foot
+	cout<<"the equivalent in cubic feet is "<<cf;
+	
+}
