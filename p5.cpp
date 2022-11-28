1a8c059a31f7068d86c311ff82d64c4607b21a95
Faisal Rashid <106532056+Faisal0619@users.noreply.github.com>
2022-11-28 22:17:29 +0530

    Add files via upload

---
+++ p5.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<ctype.h>
+using namespace std;
+int main()
+{
+	char letter;
+   cout<<"enter a letter"<<endl;
+   cin>>letter;
+   cout<< islower(letter)<<endl;
+   return 0;
+}
