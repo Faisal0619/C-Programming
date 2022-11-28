fbf40325610f7d494217ebed67431196ec24ac84
Faisal Rashid <106532056+Faisal0619@users.noreply.github.com>
2022-11-28 22:14:22 +0530

    Add files via upload

---
+++ p2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	cout<<1990<<setw(5)<<135<<endl
+	    <<1991<<setw(6)    <<7290<<endl
+	    <<1992 <<setw(7)   <<11300<<endl
+	    <<1993 <<setw(7)   <<16200<<endl;
+	     
+}
