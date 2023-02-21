d71f58fe895a084536a7870cb7a2b12f2235b234
Faisal Rashid <106532056+Faisal0619@users.noreply.github.com>
2023-02-21 23:44:13 +0530

    Add files via upload

---
+++ p11.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	cout<<setiosflags(ios::left)<<setw(12)<<"last name"<<setw(14)<<"first name"<<setw(17)<<" street address"<<setw(17)<<"town"<<setw(5)<<"state"<<endl;
+}
